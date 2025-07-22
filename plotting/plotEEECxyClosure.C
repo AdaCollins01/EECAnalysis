05b5dbb0919861fb42b6fd742937f0bcbfa099e9
Ada Collins <collinac@gw03.vm.accre.vu>
2025-07-22 16:11:27 -0500

    Normalize by NJets

--- plotting/plotEEECxyClosure.C
+++ plotting/plotEEECxyClosure.C
@@ -16,17 +16,19 @@ void plotEEECxyClosure(){
 
   bool allRL = true;
   bool logAxis = false;
-  bool partonShower = false; // Compares Simple, DIRE, VINCIA 
-  bool dataSource = true; // Compares justPythia, pythiaGeant, CMS
+  bool normalizeArea = false; // Normalizes by area. Defaults to normalizing by NJets
+  bool partonShower = true; // Compares Simple, DIRE, VINCIA 
+  bool dataSource = false; // Compares justPythia, pythiaGeant, CMS
+  bool validation = true; // Compares parton showers to CMS 
   bool invert = false; // Plots the reverse comparisons
   int rebin = 1; // Rebins histograms. Default is 1, IE no rebinning
 
-  if(partonShower + dataSource != 1) { 
-  	cout << "Please select either partonShower or dataSource." << endl;
+  if(partonShower + dataSource != 1 || dataSource + validation != 1) { 
+  	cout << "Please select either partonShower, dataSource, or validation." << endl;
 	return;
   }
 
-  TString dateFileString = "07212025";
+  TString dateFileString = "07222025";
   TString jetRadiusString = "0.8"; // Or 0.4
 
   // Bin vectors 
@@ -53,7 +55,9 @@ void plotEEECxyClosure(){
  std::vector<std::pair<const char*, const char*>> closureDataLabels;
  std::vector<TH1D*> closurePartonShower;
  std::vector<std::pair<const char*, const char*>> closurePartonShowerLabels;
- 
+ std::vector<TH1D*> closureValidation;
+ std::vector<std::pair<const char*, const char*>> closureValidationLabels;
+
    i = 0;
    for(auto RLBin : comparedRL){
      j = 0;
@@ -96,7 +100,7 @@ void plotEEECxyClosure(){
 	   	  EECHistogramManager* histogramsPythiaGeant = new EECHistogramManager(inputPythiaGeant,cardPythiaGeant); 
 	   	  EECHistogramManager* histogramsCMS = new EECHistogramManager(inputCMS,cardCMS); 
 	   	  
-	   	  int iCentrality = 0;
+		  int iCentrality = 0;
 	   	  int iEnergyEnergyCorrelatorType = EECHistogramManager::kEnergyEnergyEnergyCorrelatorFull;
 	   	  int iPairingType = EECHistograms::kSameJetPair;
 	   	  int iSubevent = EECHistograms::knSubeventCombinations;
@@ -110,15 +114,10 @@ void plotEEECxyClosure(){
 	          TH1D* PythiaGeantCMS;
 	   
 	   	      for(int iJetPt = 0; iJetPt < nJetPtBinsEEC; iJetPt++){
-	   
-	   //		std::pair<double,double> jetPtBinBorders = card->GetBinBordersJetPtEEC(iJetPt);
-	   //        	double tempNumJets = histograms->GetJetPtIntegral(iCentrality, jetPtBinBorders.first, jetPtBinBorders.second); // Gets number of jets in that jet pt bin
-	   //		normalizationFactor += tempNumJets; // Adds all jets together
-	   		
 	   		for(int iTrackPt = 0; iTrackPt < nTrackPtBinsEEC; iTrackPt++){
-	   	        hEnergyEnergyEnergyCorrelatorJustPythia[iJetPt][iTrackPt] = NULL;
-	   	        hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPt][iTrackPt] = NULL;
-	   	        hEnergyEnergyEnergyCorrelatorCMS[iJetPt][iTrackPt] = NULL;
+	   	        	hEnergyEnergyEnergyCorrelatorJustPythia[iJetPt][iTrackPt] = NULL;
+	   	        	hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPt][iTrackPt] = NULL;
+	   	        	hEnergyEnergyEnergyCorrelatorCMS[iJetPt][iTrackPt] = NULL;
 	   	         } // Track pT
 	   	      } // Jet pT
 	   
@@ -133,14 +132,20 @@ void plotEEECxyClosure(){
 	   		hEnergyEnergyEnergyCorrelatorJustPythia[iJetPtJustPythia][iTrackPtJustPythia] = histogramsJustPythia->GetHistogramEnergyEnergyEnergyCorrelatorFull(iEnergyEnergyCorrelatorType, iCentrality, iJetPtJustPythia, iTrackPtJustPythia, iPairingType, iSubevent);
 	   	
 	   		int yLowBinJustPythia = hEnergyEnergyEnergyCorrelatorJustPythia[iJetPtJustPythia][iTrackPtJustPythia]->GetYaxis()->FindBin(0.0);
-	   		int yHighBinJustPythia = hEnergyEnergyEnergyCorrelatorJustPythia[iJetPtJustPythia][iTrackPtJustPythia]->GetYaxis()->FindBin(0.9999);
+	   		int yHighBinJustPythia = hEnergyEnergyEnergyCorrelatorJustPythia[iJetPtJustPythia][iTrackPtJustPythia]->GetYaxis()->GetNbins();
 	   
 	   		// Project histograms
 	   		TH1D* projEEECJustPythia = hEnergyEnergyEnergyCorrelatorJustPythia[iJetPtJustPythia][iTrackPtJustPythia]->ProjectionX("projEEECJustPythia", yLowBinJustPythia, yHighBinJustPythia);
 	   
 	   		// Normalize them
-	   		double integralJustPythia = projEEECJustPythia->Integral("width");
-	   		projEEECJustPythia->Scale(1.0 / integralJustPythia);
+			if(normalizeArea){
+	   			double integralJustPythia = projEEECJustPythia->Integral("width");
+	   			projEEECJustPythia->Scale(1.0 / integralJustPythia);
+			} else {
+				std::pair<double,double> jetPtBinBordersJustPythia = cardJustPythia->GetBinBordersJetPtEEC(iJetPtJustPythia);
+        			double nJetsJustPythia = histogramsJustPythia->GetJetPtIntegral(iCentrality, jetPtBinBordersJustPythia.first, jetPtBinBordersJustPythia.second);
+	   			projEEECJustPythia->Scale(1.0 / nJetsJustPythia);
+			}
 			projEEECJustPythia->Rebin(rebin);
 	   	
 	   	// pythiaGeant
@@ -150,14 +155,20 @@ void plotEEECxyClosure(){
 	   		hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPtPythiaGeant][iTrackPtPythiaGeant] = histogramsPythiaGeant->GetHistogramEnergyEnergyEnergyCorrelatorFull(iEnergyEnergyCorrelatorType, iCentrality, iJetPtPythiaGeant, iTrackPtPythiaGeant, iPairingType, iSubevent);
 	   	
 	   		int yLowBinPythiaGeant = hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPtPythiaGeant][iTrackPtPythiaGeant]->GetYaxis()->FindBin(0.0);
-	   		int yHighBinPythiaGeant = hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPtPythiaGeant][iTrackPtPythiaGeant]->GetYaxis()->FindBin(0.9999);
+	   		int yHighBinPythiaGeant = hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPtPythiaGeant][iTrackPtPythiaGeant]->GetYaxis()->GetNbins();
 	   	
 	   		// Project histograms
 	   		TH1D* projEEECPythiaGeant = hEnergyEnergyEnergyCorrelatorPythiaGeant[iJetPtPythiaGeant][iTrackPtPythiaGeant]->ProjectionX("projEEECPythiaGeant", yLowBinPythiaGeant, yHighBinPythiaGeant);
 	   	
 	   		// Normalize them
-	   		double integralPythiaGeant = projEEECPythiaGeant->Integral("width");
-	   		projEEECPythiaGeant->Scale(1.0 / integralPythiaGeant);
+			if(normalizeArea){
+	   			double integralPythiaGeant = projEEECPythiaGeant->Integral("width");
+	   			projEEECPythiaGeant->Scale(1.0 / integralPythiaGeant);
+			} else {	
+				std::pair<double,double> jetPtBinBordersPythiaGeant = cardPythiaGeant->GetBinBordersJetPtEEC(iJetPtPythiaGeant);
+        			double nJetsPythiaGeant = histogramsPythiaGeant->GetJetPtIntegral(iCentrality, jetPtBinBordersPythiaGeant.first, jetPtBinBordersPythiaGeant.second);
+	   			projEEECPythiaGeant->Scale(1.0 / nJetsPythiaGeant);
+			}
 			projEEECPythiaGeant->Rebin(rebin);
 	   	
 	   	// CMS
@@ -167,14 +178,21 @@ void plotEEECxyClosure(){
 	   		hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS] = histogramsCMS->GetHistogramEnergyEnergyEnergyCorrelatorFull(iEnergyEnergyCorrelatorType, iCentrality, iJetPtCMS, iTrackPtCMS, iPairingType, iSubevent);
 	   	
 	   		int yLowBinCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->GetYaxis()->FindBin(0.0);
-	   		int yHighBinCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->GetYaxis()->FindBin(0.999);
+	   		int yHighBinCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->GetYaxis()->GetNbins();
+
 	   	
 	   		// Project histograms
 	   		TH1D* projEEECCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->ProjectionX("projEEECCMS", yLowBinCMS, yHighBinCMS);
 	   	
 	   		// Normalize them
-	   		double integralCMS = projEEECCMS->Integral("width");
-	   		projEEECCMS->Scale(1.0 / integralCMS);
+	   		if(normalizeArea){
+				double integralCMS = projEEECCMS->Integral("width");
+	   			projEEECCMS->Scale(1.0 / integralCMS);
+			} else {
+				std::pair<double,double> jetPtBinBordersCMS = cardCMS->GetBinBordersJetPtEEC(iJetPtCMS);
+        			double nJetsCMS = histogramsCMS->GetJetPtIntegral(iCentrality, jetPtBinBordersCMS.first, jetPtBinBordersCMS.second);
+	   			projEEECCMS->Scale(1.0 / nJetsCMS);
+			}
 			projEEECCMS->Rebin(rebin);
 	   	
 	   	// Calculate comparisons
@@ -217,8 +235,9 @@ void plotEEECxyClosure(){
       }	
 	
       if(partonShower){
+
 	  
-	  TString inputFileDire = "pythia/rootFiles/pythia_pp_eec_dire_6000events_07162025.root";
+	  TString inputFileDire = "pythia/rootFiles/pythia_pp_eec_direWeighted_6000events_07212025.root";
   	  TString inputFileSimple = "pythia/rootFiles/pythia_pp_eec_simple_6000events_07162025.root";
   	  TString inputFileVincia = "pythia/rootFiles/pythia_pp_eec_vincia_6000events_07162025.root";
 	 
@@ -238,39 +257,57 @@ void plotEEECxyClosure(){
 	// Simple
 		TH2F* histSimple =  (TH2F*) inputSimple->Get(histname);
 		int yLowBinSimple = histSimple->GetYaxis()->FindBin(0.0);
-		int yHighBinSimple = histSimple->GetYaxis()->FindBin(0.9999);
+		int yHighBinSimple = histSimple->GetYaxis()->GetNbins();
 	
 		// Project histograms
 		TH1D* projEEECSimple = histSimple->ProjectionX("projEEECSimple", yLowBinSimple, yHighBinSimple);
 	
 		// Normalize them
-		double integralSimple = projEEECSimple->Integral("width");
-		projEEECSimple->Scale(1.0 / integralSimple);
+		if(normalizeArea){
+			double integralSimple = projEEECSimple->Integral("width");
+			projEEECSimple->Scale(1.0 / integralSimple);
+		} else{
+			TParameter<int>* paramSimple = (TParameter<int>*) inputSimple->Get("numJets");
+			double nJetsSimple = (double) paramSimple->GetVal();
+			projEEECSimple->Scale(1.0 / nJetsSimple);
+		}
 		projEEECSimple->Rebin(rebin);
 	// Dire
 		TH2F* histDire =  (TH2F*) inputDire->Get(histname);
 		int yLowBinDire = histDire->GetYaxis()->FindBin(0.0);
-		int yHighBinDire = histDire->GetYaxis()->FindBin(0.9999);
+		int yHighBinDire = histDire->GetYaxis()->GetNbins();
 	
 		// Project histograms
 		TH1D* projEEECDire = histDire->ProjectionX("projEEECDire", yLowBinDire, yHighBinDire);
 	
 		// Normalize them
-		double integralDire = projEEECDire->Integral("width");
-		projEEECDire->Scale(1.0 / integralDire);
+		if(normalizeArea){
+			double integralDire = projEEECDire->Integral("width");
+			projEEECDire->Scale(1.0 / integralDire);
+		} else {
+			TParameter<int>* paramDire = (TParameter<int>*) inputDire->Get("numJets");
+			double nJetsDire = (double) paramDire->GetVal();
+			projEEECDire->Scale(1.0 / nJetsDire);
+		}
 		projEEECDire->Rebin(rebin);
 
 	// Vincia
 		TH2F* histVincia =  (TH2F*) inputVincia->Get(histname);
 		int yLowBinVincia = histVincia->GetYaxis()->FindBin(0.0);
-		int yHighBinVincia = histVincia->GetYaxis()->FindBin(0.9999);
-	
+		int yHighBinVincia = histVincia->GetYaxis()->GetNbins();
+			
 		// Project histograms
 		TH1D* projEEECVincia = histVincia->ProjectionX("projEEECVincia", yLowBinVincia, yHighBinVincia);
 	
 		// Normalize them
-		double integralVincia = projEEECVincia->Integral("width");
-		projEEECVincia->Scale(1.0 / integralVincia);
+		if(normalizeArea){
+			double integralVincia = projEEECVincia->Integral("width");
+			projEEECVincia->Scale(1.0 / integralVincia);
+		} else {
+			TParameter<int>* paramVincia = (TParameter<int>*) inputVincia->Get("numJets");
+			double nJetsVincia = (double) paramVincia->GetVal();
+			projEEECVincia->Scale(1.0 / nJetsVincia);
+		}
 		projEEECVincia->Rebin(rebin);
 
 	// Comparisons
@@ -313,12 +350,95 @@ void plotEEECxyClosure(){
 		closurePartonShowerLabels.push_back(std::make_pair("Simple","VINCIA"));
 		closurePartonShowerLabels.push_back(std::make_pair("DIRE","VINCIA"));
 	}
+
+	if(validation){
+		TString inputFileCMS = "pp2017/projected_xyPlane_NoTrigger_RL-All_07182025.root";
+		TFile* inputCMS = TFile::Open(inputFileCMS);
+		EECCard* cardCMS = new EECCard(inputCMS);
+		const int nCentralityBinsEEC = cardCMS->GetNCentralityBins();
+	   	const int nJetPtBinsEEC = cardCMS->GetNJetPtBinsEEC();
+	   	const int nTrackPtBinsEEC = cardCMS->GetNTrackPtBinsEEC();
+		EECHistogramManager* histogramsCMS = new EECHistogramManager(inputCMS,cardCMS);
+		int iCentrality = 0;
+	   	int iEnergyEnergyCorrelatorType = EECHistogramManager::kEnergyEnergyEnergyCorrelatorFull;
+	   	int iPairingType = EECHistograms::kSameJetPair;
+	   	int iSubevent = EECHistograms::knSubeventCombinations;
+		TH2D* hEnergyEnergyEnergyCorrelatorCMS[nJetPtBinsEEC][nTrackPtBinsEEC]; // Fill with CMS value
+		int iJetPtCMS = cardCMS->FindBinIndexJetPtEEC(jetPtBin);
+	   	int iTrackPtCMS = cardCMS->GetBinIndexTrackPtEEC(trackPtBin);
+
+   		hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS] = histogramsCMS->GetHistogramEnergyEnergyEnergyCorrelatorFull(iEnergyEnergyCorrelatorType, iCentrality, iJetPtCMS, iTrackPtCMS, iPairingType, iSubevent);
+
+   		int yLowBinCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->GetYaxis()->FindBin(0.0);
+   		int yHighBinCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->GetYaxis()->GetNbins();
+
+   		// Project histograms
+   		TH1D* projEEECCMS = hEnergyEnergyEnergyCorrelatorCMS[iJetPtCMS][iTrackPtCMS]->ProjectionX("projEEECCMS", yLowBinCMS, yHighBinCMS);
+
+   		// Normalize them
+   		if(normalizeArea){
+			double integralCMS = projEEECCMS->Integral("width");
+   			projEEECCMS->Scale(1.0 / integralCMS);
+		} else {
+			std::pair<double,double> jetPtBinBordersCMS = cardCMS->GetBinBordersJetPtEEC(iJetPtCMS);
+       			double nJetsCMS = histogramsCMS->GetJetPtIntegral(iCentrality, jetPtBinBordersCMS.first, jetPtBinBordersCMS.second);
+   			projEEECCMS->Scale(1.0 / nJetsCMS);
+		}
+		projEEECCMS->Rebin(rebin);
+
+	   // Calculate comparisons 
+	   TH1D* simpleCMS;
+	   TH1D* direCMS;
+	   TH1D* vinciaCMS;
+
+	   if(invert){
+           	simpleCMS = (TH1D*) projEEECCMS->Clone("simpleCMS");
+		simpleCMS->Divide(projEEECSimple);
+           	
+		direCMS = (TH1D*) projEEECCMS->Clone("direCMS");
+		direCMS->Divide(projEEECDire);
+		
+		vinciaCMS = (TH1D*) projEEECCMS->Clone("vinciaCMS");
+		vinciaCMS->Divide(projEEECVincia);
+
+		closureValidation.clear();
+		closureValidation.push_back(simpleCMS);
+		closureValidation.push_back(direCMS);
+		closureValidation.push_back(vinciaCMS);
+		
+		closureValidationLabels.clear();
+		closureValidationLabels.push_back(std::make_pair("CMS","Simple"));
+		closureValidationLabels.push_back(std::make_pair("CMS","DIRE"));
+		closureValidationLabels.push_back(std::make_pair("CMS","VINCIA"));
+	   } else {
+           	simpleCMS = (TH1D*) projEEECSimple->Clone("simpleCMS");
+		simpleCMS->Divide(projEEECCMS);
+           	
+		direCMS = (TH1D*) projEEECDire->Clone("direCMS");
+		direCMS->Divide(projEEECCMS);
+		
+		vinciaCMS = (TH1D*) projEEECVincia->Clone("vinciaCMS");
+		vinciaCMS->Divide(projEEECCMS);
+
+		closureValidation.clear();
+		closureValidation.push_back(simpleCMS);
+		closureValidation.push_back(direCMS);
+		closureValidation.push_back(vinciaCMS);
+		
+		closureValidationLabels.clear();
+		closureValidationLabels.push_back(std::make_pair("Simple","CMS"));
+		closureValidationLabels.push_back(std::make_pair("DIRE","CMS"));
+		closureValidationLabels.push_back(std::make_pair("VINCIA","CMS"));
+
+	   }
+	}
       }
 
       // ====================================================
       // Plot each E3C
       // ====================================================
 	
+
 	// Prepare a JDrawer for drawing purposes
 	  JDrawer *drawer = new JDrawer();
 	  if(logAxis) {drawer->SetLogY(true);}
@@ -342,12 +462,15 @@ void plotEEECxyClosure(){
 	  
 	  TString trackPtFileString = Form("T=%.1f", comparedTrackPtBin.at(0));
 	  TString ptTitleString = "";
-	  TString normalMethodString = "Area";
-	  TString normalFileString = "normalArea";
+	  TString normalMethodString = "N_{Jets}";
+	  if(normalizeArea) {normalMethodString = "Area";}
+	  TString normalFileString = "normalNJets";
+	  if(normalizeArea) {normalFileString = "normalArea";}
 	  TString compareTitleString = "";
 	  TString commentFileString = "";
 	  if(partonShower) {commentFileString = "partonShowers";}
 	  if(dataSource) {commentFileString = "dataSource";}
+	  if(validation) {commentFileString = "validation";}
 	  if(invert) {commentFileString += "_inverted";}
 	  if(rebin != 1) {commentFileString += "_rebinned";}
 	  TString logFileString = "";
@@ -357,12 +480,15 @@ void plotEEECxyClosure(){
 	  ptTitleString = Form("0 < Y < 1.0, R_{Jet} = %s, %.0f GeV < Jet p_{T} < %.0f GeV, Normalized by %s", jetRadiusString.Data(), comparedJetPtBin.at(j).first, comparedJetPtBin.at(j).second, normalMethodString.Data());
 	
 	TLegend *legend;
-	if(partonShower) {legend = new TLegend(0.83,0.38,1.01,0.67);}
+	if(partonShower && !validation) {legend = new TLegend(0.83,0.38,1.01,0.67);}
 	if(dataSource) {legend = new TLegend(0.81,0.38,0.99,0.69);}
+	
+	if(validation) {legend = new TLegend(0.81,0.38,0.99,0.72);}
+	
 	legend->SetBorderSize(0);
         legend->SetFillStyle(0); 
 
-	if(partonShower){
+	if(partonShower && !validation){
 
 		legend->SetHeader("Parton Showers", "c");
 		
@@ -418,6 +544,37 @@ void plotEEECxyClosure(){
 
 		  TString legendEntry = Form("#frac{%s}{%s}", closureDataLabels.at(iHist).first, closureDataLabels.at(iHist).second);
 		  legend->AddEntry(closureDataSource[iHist], legendEntry, "p");
+		
+		}
+	}
+	
+	if(validation){
+
+		legend->SetHeader("MC / Data", "c");
+		
+		double globalMin = 1000000.0;
+		for(auto hist : closureValidation) {
+		    double minInHist = hist->GetMinimum();
+		    if(minInHist < globalMin) globalMin = minInHist;
+		}
+		//closureValidation[0]->SetMinimum(globalMin * 0.9); // 0.9 for buffer
+
+		closureValidation[0]->SetMinimum(0); 
+		closureValidation[0]->SetMaximum(5); 
+
+		for(int iHist = 0; iHist < closureValidation.size(); iHist ++){
+	   	   closureValidation[iHist]->SetLineColor(color[iHist]);
+                   closureValidation[iHist]->SetMarkerStyle(markerStyle[iHist]);
+                   closureValidation[iHist]->SetMarkerColor(color[iHist]);
+                   closureValidation[iHist]->SetLineWidth(1);
+		   if(iHist == 0){
+		   	drawer->DrawHistogram(closureValidation[iHist], "X", "Ratio", ptTitleString);
+		   } else {
+		        closureValidation[iHist]->Draw("SAME");
+		   }
+
+		  TString legendEntry = Form("#frac{%s}{%s}", closureValidationLabels.at(iHist).first, closureValidationLabels.at(iHist).second);
+		  legend->AddEntry(closureValidation[iHist], legendEntry, "p");
 		
 		}
 	}
